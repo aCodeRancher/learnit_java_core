79f5a616a5db6b6ad17bbc018870c4acc2666af9
helen <hhma@alumni.cpp.edu>
2022-01-20 14:01:25 -0500

    add account builder spy test.

--- test/com/itbulls/learnit/javacore/mockito/AccountTest.java
+++ test/com/itbulls/learnit/javacore/mockito/AccountTest.java
@@ -8,6 +8,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
 
 @ExtendWith(MockitoExtension.class)
 public class AccountTest {
@@ -18,9 +21,21 @@ public class AccountTest {
     @Mock(answer= Answers.RETURNS_DEEP_STUBS)
     private Account.Builder builder;
 
+
+    private static Account.Builder builder1;
     @Test
     public void deepStubTest(){
         Mockito.when(builder.setToken("12345").setUserId("9876").build()).thenReturn(expectedAccount);
         assertEquals(expectedAccount,builder.setToken("12345").setUserId("9876").build());
+    }
+
+
+    @Test
+    public void spyAccount(){
+        Account.Builder builder = Account.newBuilder();
+        Account.Builder spyBuilder = spy(builder);
+        Account acc = builder.setToken("6").setUserId("6").build();
+        doReturn(acc).when(spyBuilder).build();
+        assertEquals(acc, spyBuilder.build());
     }
 }
