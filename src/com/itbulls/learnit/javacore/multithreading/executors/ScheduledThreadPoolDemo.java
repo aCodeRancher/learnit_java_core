dbcd972e13fc8e79a8d6ce481a5abb613b6f8b59
helen <hhma@alumni.cpp.edu>
2022-01-26 14:24:49 -0500

    add more schedules

--- src/com/itbulls/learnit/javacore/multithreading/executors/ScheduledThreadPoolDemo.java
+++ src/com/itbulls/learnit/javacore/multithreading/executors/ScheduledThreadPoolDemo.java
@@ -1,5 +1,6 @@
 package com.itbulls.learnit.javacore.multithreading.executors;
 
+import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
@@ -7,8 +8,16 @@ public class ScheduledThreadPoolDemo {
 
 	public static void main(String[] args) {
 		var es = Executors.newScheduledThreadPool(4);
-		es.schedule(() -> System.out.println("Schedule example: " + 
-							Thread.currentThread().getName()), 1, TimeUnit.SECONDS);
+		es.schedule(() -> System.out.println("Schedule example 1 : " +
+							Thread.currentThread().getName() + System.currentTimeMillis()), 1, TimeUnit.SECONDS);
+		es.schedule(() -> System.out.println("Schedule example 2: " +
+				Thread.currentThread().getName()+ System.currentTimeMillis() ), 1, TimeUnit.SECONDS);
+		es.schedule(() -> System.out.println("Schedule example 3 : " +
+				Thread.currentThread().getName()+  System.currentTimeMillis()), 1, TimeUnit.SECONDS);
+		es.schedule(() -> System.out.println("Schedule example 4: " +
+				Thread.currentThread().getName()+ System.currentTimeMillis()), 1, TimeUnit.SECONDS);
+		es.schedule(() -> System.out.println("Schedule example 5: " +
+				Thread.currentThread().getName()+ System.currentTimeMillis()), 1, TimeUnit.SECONDS);
 		es.shutdown();
 		
 		
