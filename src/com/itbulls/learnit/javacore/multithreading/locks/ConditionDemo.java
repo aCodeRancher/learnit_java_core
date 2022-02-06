ca94ccf35c265dedcadb509a2c8937d03ef44215
helen <hhma@alumni.cpp.edu>
2022-02-06 12:32:27 -0500

    let the push and pop run in a loop.

--- src/com/itbulls/learnit/javacore/multithreading/locks/ConditionDemo.java
+++ src/com/itbulls/learnit/javacore/multithreading/locks/ConditionDemo.java
@@ -2,7 +2,9 @@ package com.itbulls.learnit.javacore.multithreading.locks;
 
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Random;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -18,11 +20,20 @@ public class ConditionDemo {
 
 	public static void main(String[] args) {
 		var demo = new ConditionDemo();
-		var es = Executors.newFixedThreadPool(4);
-		
-		es.submit(() -> System.out.println(demo.popFromStack()));
-		es.submit(() -> demo.pushToStack("Test String #1"));
-		
+		var es = Executors.newFixedThreadPool(6);
+		Random randomNo = new Random();
+		int generatedNo = 0;
+		AtomicInteger atomicInteger = new AtomicInteger();
+		for (int i = 0 ; i<10; i++) {
+			generatedNo = randomNo.nextInt(10);
+			atomicInteger.set(generatedNo);
+			es.submit(() -> demo.pushToStack(Integer.toString(atomicInteger.get())));
+
+
+		}
+        for (int i=0;i<8;i++)
+		   es.submit(()-> demo.popFromStack());
+
 		es.shutdown();
 	}
 
@@ -33,6 +44,7 @@ public class ConditionDemo {
 				stackFullCondition.await();
 			}
 			stack.push(item);
+			System.out.println("push : " + item + " stack size: "+ stack.size());
 			stackEmptyCondition.signalAll();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -48,14 +60,19 @@ public class ConditionDemo {
 			while (stack.size() == 0) {
 				stackEmptyCondition.await();
 			}
-			return stack.pop();
+			System.out.println("pop out : " + stack.peek());
+
+			stack.pop();
+			System.out.println(" full signal all ");
+			stackFullCondition.signalAll();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} finally {
-			stackFullCondition.signalAll();
+			//stackFullCondition.signalAll();
+			System.out.println("pop release lock, current size: "+ stack.size());
 			lock.unlock();
 		}
-		return "";
+		return "current size : " + stack.size();
 	}
 
 }
