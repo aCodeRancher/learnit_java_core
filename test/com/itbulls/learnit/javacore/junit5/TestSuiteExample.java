47b013ef0aeed74187daa1fab2644840a3a7a14f
helen <hhma@alumni.cpp.edu>
2022-05-08 20:50:19 -0400

    comment out some lines

--- test/com/itbulls/learnit/javacore/junit5/TestSuiteExample.java
+++ test/com/itbulls/learnit/javacore/junit5/TestSuiteExample.java
@@ -1,19 +1,15 @@
 package com.itbulls.learnit.javacore.junit5;
 
-import org.junit.platform.suite.api.ExcludePackages;
-import org.junit.platform.suite.api.ExcludeTags;
-import org.junit.platform.suite.api.IncludePackages;
-import org.junit.platform.suite.api.IncludeTags;
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.runner.RunWith;
 import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runners.Suite.SuiteClasses;
+
+import org.junit.platform.suite.api.*;
+import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
-
+//@Suite
+//@SuiteDisplayName("A demo test")
 //By default, it will only include test classes whose names either begin with Test or end with Test or Tests.
-@SelectPackages("com.itbulls.learnit.javacore.junit5") 
+@SelectPackages("com.itbulls.learnit.javacore.junit5")
 //@SelectPackages({"com.itbulls.learnit.javacore.junit5","com.itbulls.learnit.javacore.jcf"})
 
 //@SelectPackages("com.itbulls.learnit.javacore") 
@@ -23,8 +19,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @IncludeTags("production")
 //@ExcludeTags("development")
 
-@SelectClasses( JUnitApiAdvancedExamples.class )
-//@SelectClasses( {CalculatorTests.class, MoneyTransactionServiceTest.class} )
+@SelectClasses(JUnitApiAdvancedExamples.class)
+  //@SelectClasses( {CalculatorTests.class, MoneyTransactionServiceTest.class} )
 public class TestSuiteExample {
 
 }
